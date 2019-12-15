055100c510af8abc0ca4ca9a97f5aba90ad79005
Neuenmuller <greg870601@gmail.com>
2019-12-16 01:58:41 +0800

    initial push

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+import setuptools
+
+setuptools.setup(
+    name='line_broadcaster',
+    version='1.0_b',
+    keywords='demo',
+    description='A demo for python packaging.',
+    long_description="nah",
+    author='ST Chien',
+    author_email='greg870601@gmail.com',
+
+    url='',
+    packages=setuptools.find_packages(),
+    license='MIT'
+)
