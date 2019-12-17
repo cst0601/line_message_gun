79cf284616af4a324f2410f49b17453f00eb49fd
Neuenmuller <greg870601@gmail.com>
2019-12-18 00:14:00 +0800

    fix error that does not reflect on local

--- line_broadcaster/line_broadcaster.py
+++ line_broadcaster/line_broadcaster.py
@@ -29,7 +29,7 @@ bp = Blueprint("broadcaster", __name__, url_prefix="/line")
 def callback():
     signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
-    app.logger.info("Request body:" + body)
+    bp.logger.info("Request body:" + body)
 
     #handle webhook body
     try:
